2dbdc3f183ae596d01614611a03b4dd45df2910a
AdamGalla <73466420+AdamGalla@users.noreply.github.com>
2021-05-12 10:34:17 +0200

    sidebar to AllVideos

--- src/guilayer/AllVideos.java
+++ src/guilayer/AllVideos.java
@@ -18,6 +18,9 @@ import java.awt.Choice;
 import java.awt.Color;
 import javax.swing.JComboBox;
 import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AllVideos extends JFrame {
 
@@ -60,18 +63,43 @@ public class AllVideos extends JFrame {
 		JPanel panel = new JPanel();
 		scrollPane.setViewportView(panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{20, 54, 118, 156, 57, 0};
-		gbl_panel.rowHeights = new int[]{10, 74, 70, 43, 45, 50, 200, 230};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0};
+		gbl_panel.columnWidths = new int[]{46, 118, 156, 57, 0};
+		gbl_panel.rowHeights = new int[]{44, 0, 74, 70, 43, 45, 50, 200, 230};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0};
 		panel.setLayout(gbl_panel);
+		
+	
+		SideBarCustomer sidebar = new SideBarCustomer();
+		sidebar.setSize(0, 740);
+		contentPane.add(sidebar);
+		sidebar.setVisible(false);
+			
+		JButton sidebarButton = new JButton("");
+		sidebarButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sidebar.setVisible(true);
+				sidebar.setSize(225, 740);
+			}
+		});
+		sidebarButton.setIcon(new ImageIcon(AllVideos.class.getResource("/images/sidebarIcon35px.png")));
+		sidebarButton.setOpaque(false);
+		sidebarButton.setForeground(Color.BLACK);
+		sidebarButton.setBorderPainted(false);
+		sidebarButton.setBackground(Color.LIGHT_GRAY);
+		GridBagConstraints gbc_sidebarButton = new GridBagConstraints();
+		gbc_sidebarButton.insets = new Insets(0, 0, 5, 5);
+		gbc_sidebarButton.gridx = 0;
+		gbc_sidebarButton.gridy = 0;
+		panel.add(sidebarButton, gbc_sidebarButton);
 		
 		logoImage = new JLabel(new ImageIcon(VideoCategories.class.getResource("/images/logo.png")));
 		GridBagConstraints gbc_logoImage = new GridBagConstraints();
+		gbc_logoImage.gridheight = 2;
 		gbc_logoImage.gridwidth = 3;
 		gbc_logoImage.anchor = GridBagConstraints.NORTHEAST;
 		gbc_logoImage.insets = new Insets(0, 0, 5, 5);
-		gbc_logoImage.gridx = 1;
+		gbc_logoImage.gridx = 0;
 		gbc_logoImage.gridy = 1;
 		panel.add(logoImage, gbc_logoImage);
 		
@@ -80,16 +108,16 @@ public class AllVideos extends JFrame {
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.gridwidth = 2;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 2;
-		gbc_lblNewLabel.gridy = 3;
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 4;
 		panel.add(lblNewLabel, gbc_lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Sort by:");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 1;
-		gbc_lblNewLabel_1.gridy = 4;
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 5;
 		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		String[] filterTypes = {"Newest added", "Oldest added", "Most watched"};
@@ -97,8 +125,8 @@ public class AllVideos extends JFrame {
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-		gbc_comboBox.gridx = 2;
-		gbc_comboBox.gridy = 4;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 5;
 		panel.add(comboBox, gbc_comboBox);
 		
 		JPanel panel_1 = new JPanel();
@@ -106,8 +134,8 @@ public class AllVideos extends JFrame {
 		gbc_panel_1.gridwidth = 2;
 		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 2;
-		gbc_panel_1.gridy = 6;
+		gbc_panel_1.gridx = 1;
+		gbc_panel_1.gridy = 7;
 		panel.add(panel_1, gbc_panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
 		gbl_panel_1.columnWidths = new int[]{301, 0};
