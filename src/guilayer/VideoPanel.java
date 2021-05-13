242154793f7d3026c505d4cf0734941371e46c3f
Tomas Chmelik <chmelik.tomas@email.cz>
2021-05-13 15:13:31 +0200

    VideoPanel

---
+++ src/guilayer/VideoPanel.java
@@ -0,0 +1,69 @@
+package guilayer;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+import controllayer.ManageVideoController;
+import modellayer.Video;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VideoPanel extends JPanel {
+	
+	private ManageVideoController manageVideoController = new ManageVideoController();
+
+	/**
+	 * Create the panel.
+	 */
+	public VideoPanel(Video video) {
+		setBackground(Color.ORANGE);
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(11, 42, 378, 245);
+		add(panel);
+		
+		JLabel nameLabel = new JLabel("");
+		nameLabel.setText(video.getHeader());
+		nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		nameLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		nameLabel.setBounds(11, 19, 378, 13);
+		add(nameLabel);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setText(video.getShortDescription());
+		textPane.setEditable(false);
+		textPane.setBounds(11, 295, 345, 58);
+		add(textPane);
+		
+		JButton editVideoButton = new JButton("");
+		editVideoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EditVideo editVideo = new EditVideo(video);
+				editVideo.setVisible(true);
+				((JFrame) getTopLevelAncestor()).dispose();
+			}
+		});
+		editVideoButton.setOpaque(false);
+		editVideoButton.setBorderPainted(false);
+		editVideoButton.setIcon(new ImageIcon(VideoPanel.class.getResource("/images/editVideoButton.png")));
+		editVideoButton.setBounds(366, 297, 23, 21);
+		add(editVideoButton);
+		
+		JButton deleteVideoButton = new JButton("");
+		deleteVideoButton.setIcon(new ImageIcon(VideoPanel.class.getResource("/images/deleteButton.png")));
+		deleteVideoButton.setOpaque(false);
+		deleteVideoButton.setBorderPainted(false);
+		deleteVideoButton.setBounds(366, 332, 23, 21);
+		add(deleteVideoButton);
+
+	}
+}
